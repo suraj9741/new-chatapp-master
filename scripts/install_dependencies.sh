2b8b532b676478043c847640c15779a9454c984c
Suraj <39755990+suraj9741@users.noreply.github.com>
2021-01-09 08:51:59 +0530

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3-pip python-dev nginx git
+sudo apt-get update
+sudo pip3 install virtualenv
+virtualenv venv
+source venv/bin/activate
+cd new_chatapp																
+pip3 install -r requirements.txt
+pip3 install django bcrypt django-extensions
+pip3 install gunicorn
