5cfb61c0ce5a10aac903620883bd4bf1cf16eada
Suraj <39755990+suraj9741@users.noreply.github.com>
2021-01-09 19:50:25 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,9 +4,10 @@ sudo apt-get update
 sudo apt-get -y install python3-pip python-dev nginx git
 sudo apt-get update
 sudo pip3 install virtualenv
+cd /home/ubuntu/
 virtualenv venv
-source venv/bin/activate
-cd new_chatapp																
+source /home/ubuntu/venv/bin/activate
+cd /home/ubuntu/new_chatapp																
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
