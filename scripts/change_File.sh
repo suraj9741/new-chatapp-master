1811f6f488a8298badeb1ada1da5dcbe263861f3
Suraj <39755990+suraj9741@users.noreply.github.com>
2021-01-11 00:54:05 +0530

    Update change_File.sh

--- scripts/change_File.sh
+++ scripts/change_File.sh
@@ -2,7 +2,6 @@
 
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
-#sudo rm /etc/nginx/sites-available/fundoo.conf
 sudo ln -s /etc/nginx/sites-available/fundo /etc/nginx/sites-enabled
 sudo rm /etc/nginx/sites-available/default
 sudo nginx -t
