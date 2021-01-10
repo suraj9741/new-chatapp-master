4375442f5c8c9591cc48ded00f03493272a990de
Suraj <39755990+suraj9741@users.noreply.github.com>
2021-01-11 02:37:02 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,6 +8,6 @@ cd /home/ubuntu/
 virtualenv env
 source /home/ubuntu/env/bin/activate
 cd /home/ubuntu/new_chatapp-master															
-pip3 install -r requirements.txt
+pip3 install -r /home/ubuntu/new_chatapp-master/requirements.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
