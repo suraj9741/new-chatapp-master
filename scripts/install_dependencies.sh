2d5148055b9533e08833e9173d9f01cc80e81765
Suraj <39755990+suraj9741@users.noreply.github.com>
2021-01-12 00:51:37 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y install python3-pip python-dev nginx git
-sudo apt-get update
-sudo pip3 install virtualenv
-cd /home/ubuntu/
-virtualenv env
 source /home/ubuntu/env/bin/activate
 cd /home/ubuntu/new_chatapp-master		
 pip3 install -r /home/ubuntu/new_chatapp-master/requirements.txt
